814d1e55aa7bc4776253ae83859a6728ec457e15
ichachy <ichachy.lightricks.com>
2021-09-05 17:50:12 +0300

    Implementation of Network util
    Added implememtation of classes that are responsible of parsering a JSON source of data and
    map it's data to a DTO object. The code requires Retrofit, Moshi, RxJava libraries

--- app/src/main/java/com/lightricks/feedexercise/network/DataTransferObjects.kt
+++ app/src/main/java/com/lightricks/feedexercise/network/DataTransferObjects.kt
@@ -1,5 +1,21 @@
 package com.lightricks.feedexercise.network
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
 /**
- * todo: add Data Transfer Object data class(es) here
+ * DTO class. Maps a JSON into a list of [TemplatesMetadataItem]
  */
+@JsonClass(generateAdapter = true)
+data class GetFeedResponse(
+    @Json(name = "templatesMetadata") val templatesMetadataList: List<TemplatesMetadataItem>)
+
+@JsonClass(generateAdapter = true)
+data class TemplatesMetadataItem(
+        @Json(name = "configuration") val configuration: String,
+        @Json(name = "id") val id: String,
+        @Json(name = "isNew") val isNew: Boolean,
+        @Json(name = "isPremium") val isPremium: Boolean,
+        @Json(name = "templateCategories") val templateCategories: List<String>,
+        @Json(name = "templateName") val templateName: String?,
+        @Json(name = "templateThumbnailURI") val templateThumbnailURI: String)
