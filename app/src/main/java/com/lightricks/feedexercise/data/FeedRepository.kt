4b865fad41db54d70244e02272efa0af9b2f498e
ichachy <ichachy.lightricks.com>
2021-09-05 18:10:53 +0300

    Implementation of Repository
    Added an implementation of Repository class. The repository will fetch data from network and save it in a local database. A test classes were added for the repository

--- app/src/main/java/com/lightricks/feedexercise/data/FeedRepository.kt
+++ app/src/main/java/com/lightricks/feedexercise/data/FeedRepository.kt
@@ -1,9 +1,45 @@
 package com.lightricks.feedexercise.data
 
-/**
- * This is our data layer abstraction. Users of this class don't need to know
- * where the data actually comes from (network, database or somewhere else).
- */
-class FeedRepository {
-    //todo: implement
+import com.lightricks.feedexercise.database.FeedDao
+import com.lightricks.feedexercise.database.FeedEntity
+import com.lightricks.feedexercise.network.FeedApiService
+import com.lightricks.feedexercise.network.GetFeedResponse
+import com.lightricks.feedexercise.network.TemplatesMetadataItem
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Flowable
+
+class FeedRepository(private val feedApiService: FeedApiService,
+                     private val feedDao: FeedDao) {
+
+    fun getAll(): Flowable<List<FeedItem>>{
+        return feedDao.getAll().map {
+            it.toFeedItems()
+        }
+    }
+
+    private fun List<FeedEntity>.toFeedItems(): List<FeedItem> {
+        return map {
+            FeedItem(it.id, PREFIX_URL + it.thumbnailUrl, it.isPremium)
+        }
+    }
+
+    fun refresh() : Completable{
+        return feedApiService.getFeed()
+            .flatMapCompletable { feedResponse -> updateDatabase(feedResponse) }
+    }
+
+    private fun updateDatabase(feedResponse: GetFeedResponse): Completable {
+        return feedDao.deleteAllAndInsert(feedResponse.templatesMetadataList.toFeedEntities())
+    }
+
+    private fun List<TemplatesMetadataItem>.toFeedEntities() : List<FeedEntity> {
+        return map {
+            FeedEntity(it.id, it.templateThumbnailURI, it.isPremium)
+        }
+    }
+
+    companion object{
+        private const val PREFIX_URL = "https://assets.swishvideoapp.com/" +
+                "Android/demo/catalog/thumbnails/"
+    }
 }
